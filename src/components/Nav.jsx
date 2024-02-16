ebbb2c90d5972eaf2f965a53bce6638ce98701ff
Shivam <shivamkonline@gmail.com>
2024-02-16 21:31:23 +0530

    folder structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import {headerLogo} from '../assets/images';
-import {hamburger} from '../assets/icons';
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import {navLinks} from '../constants';
 
 const Nav = () => {
     return (
-        <header>
-            <nav>
+        <header className='padding-x py-8 absolute z-10 w-full'>
+            <nav className='flex justify-between items-center max-container'>
                 <a href="/">
-                    <img src="{headerLogo}" alt="Nike Logo" />
-                    </a>     
+                    <img src={ headerLogo } alt="Nike Logo" width={130} height={29} />
+                </a>
+                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                    {navLinks.map((item)=>(
+                        <li key={item.label}>
+                            <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>     
             </nav>
 
         </header>
