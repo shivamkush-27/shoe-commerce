498b27f8007be8d36cd5fbf36dc7e88e28edc414
Shivam <shivamkonline@gmail.com>
2024-02-20 21:10:22 +0530

    Popular Product Section
    Added a new section PopularProducts

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { products } from '../constants';
+import PopularProductCard from '../components/PopularProductCard';
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id='products' className='max-container max-sm:mt-12'>
+      <div className='flex flex-col justify-start gap-5'>
+        <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
+        <p className='mt-2 lg:max-w-lg font-montserrat text-slate-gray'>Experience the top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value.</p>
+      </div>
+      <div className='mt-8 grid lg:grid-cols-4 md:grid-col-3 sm:grid-cols-2 grid-cols-1 gap-14'>
+        {products.map((product)=>(
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
   )
 }
 
