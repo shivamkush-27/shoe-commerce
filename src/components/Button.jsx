f42c215bf1a018d3cb850ede81680f981db6b6f8
Shivam <shivamkonline@gmail.com>
2024-02-21 21:10:06 +0530

    Third Section Added
    Super Quality Section added to the homepage

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({label, iconUrl}) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>{label}
-    <img src={iconUrl} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5' />
+    {iconUrl && <img src={iconUrl} alt="arrow right icon" className='ml-2 rounded-full w-5 h-5' />}
     </button>
   )
 }
